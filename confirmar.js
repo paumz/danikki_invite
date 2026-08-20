abff8922ed11b7ea07aff1b18fef1bf52d269697
paumz <paulina13munoz@gmail.com>
2026-08-20 17:56:20 -0600

    Update confirmar.js
    confirmar

--- confirmar.js
+++ confirmar.js
@@ -9,45 +9,32 @@ const pregunta = document.querySelector(".pregunta");
 
 
 /* Nombre */
-
 if (nombre) {
     nombreInvitado.textContent = nombre;
 }
 
 
-/* Número de invitados */
-
+/* invitados */
 if (invitados === 1) {
-
     mensajeInvitados.innerHTML = `
         Hemos reservado <strong>1 lugar</strong> para ti.
     `;
-
     pregunta.textContent =
         "¿Tendremos el placer de contar contigo?";
-
 } else {
-
     mensajeInvitados.innerHTML = `
         Hemos reservado <strong>${invitados} lugares</strong> para ustedes.
     `;
-
     pregunta.textContent =
         "¿Tendremos el placer de contar con ustedes?";
 }
 
 
-/* Botones */
-
+/* buttonssss */
 document.getElementById("confirmar-si").addEventListener("click", () => {
-
     console.log("Asistencia confirmada");
-
 });
 
-
 document.getElementById("confirmar-no").addEventListener("click", () => {
-
     console.log("Asistencia rechazada");
-
 });
