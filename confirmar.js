cadbd3490a42bfa0b7d4beb850ad5aadeff374cf
paumz <paulina13munoz@gmail.com>
2026-08-20 18:17:17 -0600

    Update confirmar.js
    excec

--- confirmar.js
+++ confirmar.js
@@ -32,7 +32,7 @@ if (invitados === 1) {
 
 
 /* buttonssss */
-const URL_SCRIPT = "https://script.google.com/macros/s/AKfycbxgmkLPfpn2eejU5JMiitdyNBxiqfmUv23Swn0L6ucEDuYjWF5tTlk3i-er7F1NFAn94g/exec";
+const URL_SCRIPT = "https://script.google.com/macros/s/AKfycbx-e7rL_SKOaAWkc36pPqx7-tipT_CurY7Gd-UTrNmu0RbHeUHTvkMiNd0xlAJ3Oec2Vg/exec";
 function registrarRespuesta(respuesta) {
     const datos = {
         id: id,
